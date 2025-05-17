5507500a00aea30cc434c1f6250268eec4f157af
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-17 18:58:42 +0300

    Revert "Battleship api"

--- src/errors/input-validation-error.ts
+++
@@ -1,7 +0,0 @@
-export default class InputValidationError extends Error {
-  code: string;
-  constructor(message: string) {
-    super(message);
-    this.code = 'IVErr';
-  }
-}
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { httpServer } from './http_server/index';
-import App from './app';
-const HTTP_PORT = 8181;
-
-//eslint-disable-next-line no-console
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
-const app = new App();
-app.start();
